fff418429498e9af4e1aabb2b398ae2d4cc5393e
Satyajeet Yadav <syadav@metaapply.io>
2026-03-30 16:44:02 +0530

    metaflydestina

--- frontend/app/metainsure/page.tsx
+++ frontend/app/metainsure/page.tsx
@@ -1,17 +1,16 @@
 import Section from '@/components/common/Section';
 import SectionHeader from '@/components/common/SectionHeader';
 import FAQSection from '@/components/sections/FAQSection';
-
+import ServingDestinations from '@/components/sections/ServicesPageDestinations';
 import SuccessStories from '@/components/sections/SuccessStories';
 
 export default function MetaFlyPage() {
     return (
         <>
-            {/* Success Story */}
-            <Section spacing="lg">
-                <SuccessStories page="metainsure" type="testimonial" />
-            </Section>
 
+            {/* Success Story */}
+            <ServingDestinations />
+            <SuccessStories page="metainsure" type="testimonial" />
             <FAQSection page="metainsure" />
         </>
     );
