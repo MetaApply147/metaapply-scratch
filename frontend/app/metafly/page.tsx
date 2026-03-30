e2ba6444f4248a6b9525836670ffddb1e6fb06f1
Satyajeet Yadav <syadav@metaapply.io>
2026-03-30 14:34:27 +0530

    metaflydestination

--- frontend/app/metafly/page.tsx
+++ frontend/app/metafly/page.tsx
@@ -6,13 +6,8 @@ export default function MetaFlyPage() {
   return (
     <>
       {/* Success Story */}
-<<<<<<< HEAD
       <ServingDestinations />
       <SuccessStories page="metafly" type="testimonial" />
-=======
-        <SuccessStories page="metafly" type="testimonial" />
-
->>>>>>> 695b424b090aa68bb40a469c5ee015e6955f1fd2
       <FAQSection page="metafly" />
 
 
