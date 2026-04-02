3613a91b4098cb9ad60ea2f7771dfc5fbb6cc110
Ekta Gupta <ektaguptaa858@gmail.com>
2026-04-02 13:14:31 +0530

    Added console for key

--- frontend/app/api/leads/route.ts
+++ frontend/app/api/leads/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
-
+console.log("SECRET KEY:", process.env.RECAPTCHA_SECRET_KEY ?? "UNDEFINED");
     try {
         const body = await req.json();
         console.log("BODY:", JSON.stringify(body));
