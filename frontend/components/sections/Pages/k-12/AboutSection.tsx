85c2512bbf2e5dbb52574841f83dc1e086791dd1
Ekta Gupta <ektaguptaa858@gmail.com>
2026-04-10 15:34:24 +0530

    Content changes of k12

--- frontend/components/sections/Pages/k-12/AboutSection.tsx
+++ frontend/components/sections/Pages/k-12/AboutSection.tsx
@@ -6,17 +6,17 @@ import { Box, Typography } from "@mui/material";
 const cards = [
   {
     title: "Identifying the Challenges",
-    body: "Parents often find it challenging to locate schools that provide academic excellence, global exposure, holistic development, and a nurturing environment for each child's unique potential. MetaApply understands these concerns and aims to bridge the gap.",
+    body: "Parents often find it challenging to locate schools that provide academic excellence, global exposure, holistic development, and a nurturing environment for each child’s unique potential. MetaApply understands these concerns and aims to bridge the gap.",
     bg: "#335C67",
   },
   {
     title: "Partnerships with Top Global Schools",
-    body: "We have partnered with top international schools to offer your child access to globally-recognised education systems that prioritise critical thinking, creativity, and a global perspective. These schools provide a rich combination of academics, co-curricular activities, and state-of-the-art facilities.",
+    body: "We have partnered with top international schools to offer your child access to globally recognised education systems that prioritise critical thinking, creativity, and a global perspective. These schools provide a rich combination of academics, co-curricular activities, and state-of-the-art facilities.",
     bg: "#73291F",
   },
   {
     title: "Comprehensive Support",
-    body: "Similar to our approach for higher education, our expert counsellors will collaborate with you to understand your child's needs and aspirations. We will help you shortlist the best schools, examining highlights into curriculum differences, school strengths, and the admissions process.",
+    body: "Similar to our approach for higher education, our expert counselors will collaborate with you to understand your child's needs and aspirations. We will help you shortlist the best schools, offering insights into curriculum differences, school strengths, and the admissions process.",
     bg: "#A27328",
   },
 ];
@@ -35,12 +35,7 @@ export default function AboutSection() {
                     color: 'common.white'
                 }}
                 >
-                At MetaApply, we've established a strong reputation for guiding students through their
-                higher education journeys abroad. Understanding the challenges parents and students face
-                in finding premium education in India, we have expanded our services to K-12 education,
-                where we cater to education from Kindergarten to class 12th. By partnering with top
-                schools in India and abroad, we strive to help families secure high-quality education
-                for their children.
+                At MetaApply, we’ve established a strong reputation for guiding students through their higher education journeys abroad. Understanding the challenges parents and students face in finding premium education in India, we have expanded our services to K-12 education, where we cater education from kindergarten to class 12th. By partnering with top schools in India and abroad, we strive to help families secure high-quality education for their children.
             </Typography>
         </Section>
 
