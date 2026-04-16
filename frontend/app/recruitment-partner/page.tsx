eb4181c0761a22c90f7599cde19abc35e31fa3f7
Satyajeet Yadav <syadav@metaapply.io>
2026-04-16 15:48:25 +0530

    recrut partner

--- frontend/app/recruitment-partner/page.tsx
+++ frontend/app/recruitment-partner/page.tsx
@@ -2,6 +2,8 @@ import HeroBanner from '@/components/banner/HeroBanner';
 import FAQSection from '@/components/sections/FAQSection';
 import SuccessStories from '@/components/sections/SuccessStories';
 import PopularDestinations from '@/components/sections/PopularDestinations';
+import ServicesSection from '@/components/sections/ServicesSection';
+import PartneredUniversities from '@/components/sections/PartneredUniversities';
 import { baseFields } from '@/config/forms/base.fields';
 
 export default function RecruitmentPartnerPage() {
@@ -10,7 +12,11 @@ export default function RecruitmentPartnerPage() {
         <>
             <HeroBanner slug="recruitment-partner" size="medium" />
 
+            {/* Partnered Universities */}
+            <PartneredUniversities />
 
+            {/* Our Services */}
+            <ServicesSection />
 
             {/* Popular Destination section */}
             <PopularDestinations bgColor='#F7FBFF' />
