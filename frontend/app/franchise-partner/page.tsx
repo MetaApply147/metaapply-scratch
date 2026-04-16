a4358c23ee22f04afd25daf30a605f9447d26712
Satyajeet Yadav <syadav@metaapply.io>
2026-04-16 15:57:20 +0530

    franchise partner

---
+++ frontend/app/franchise-partner/page.tsx
@@ -0,0 +1,14 @@
+import HeroBanner from '@/components/banner/HeroBanner';
+
+
+import { baseFields } from '@/config/forms/base.fields';
+
+export default function MetaapplyAmplifyPage() {
+
+    return (
+        <>
+            <HeroBanner slug="franchise-partner" size="medium" />
+        </>
+    );
+}
+
