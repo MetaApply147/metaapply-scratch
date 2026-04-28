ec75ec3c5c311d3c1a0013d01c3206c19cd24edb
Ekta Gupta <ektaguptaa858@gmail.com>
2026-04-28 12:31:46 +0530

    Middle ware change

--- metaapply-backend/config/middlewares.ts
+++ metaapply-backend/config/middlewares.ts
@@ -9,7 +9,8 @@ const config: Core.Config.Middlewares = [
     config: {
       origin: [
         "http://localhost:3000",
-        "https://next-frontend-hwa6c8h5cbhgfkh2.centralindia-01.azurewebsites.net"
+        "https://next-frontend-hwa6c8h5cbhgfkh2.centralindia-01.azurewebsites.net",
+        "https://staging.metaapply.io"
       ],
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
       headers: '*',
