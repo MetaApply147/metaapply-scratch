2ecb3f8dd28033c7ab758334920d84cd128896a8
Ekta Gupta <ektaguptaa858@gmail.com>
2026-05-14 18:37:36 +0530

    Updated strcuture

--- frontend/app/centre/[slug]/page.tsx
+++ frontend/app/centre/[slug]/page.tsx
@@ -1,36 +1,11 @@
-import { fetchEvents } from "@/services/events";
-import EventHero from "@/components/sections/Pages/eventinner/EventHero";
-import EventMainSection from "@/components/sections/Pages/eventinner/EventMainSection";
-import PastEvents from "@/components/sections/Pages/eventinner/PastEvents";
+import HeroBanner from "@/components/banner/HeroBanner";
 
-type Props = {
-  params: Promise<{ slug: string }>;
-};
-
-const EventDetailPage = async ({ params }: Props) => {
-  const { slug } = await params;
-
-  const events = await fetchEvents();
-
-  const event = events.find(
-    (item: any) => item.slug?.toLowerCase() === slug?.toLowerCase()
-  );
-
-  if (!event) return <div>Event not found</div>;
-
+const CentreDetailPage = () => {
   return (
     <>
-      <EventHero event={event} />
-
-      <EventMainSection event={event} />
-
-      <PastEvents
-        events={events.filter(
-          (item: any) => item.slug !== event.slug
-        )}
-      />
+        <HeroBanner/>      
     </>
   );
 };
 
-export default EventDetailPage;
+export default CentreDetailPage;
